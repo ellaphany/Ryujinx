ece9a82ce6c974eb821faa17bf05918c36c11286
ellaphany <77451529+ellaphany@users.noreply.github.com>
2021-04-02 22:49:59 +0800

    Revert "Merge branch 'pr/2155'"
    This reverts commit fa21fecb378aed5206a18f50816763fe5bc12caa, reversing
    changes made to 73175dd60b4c13dbf0fd7cb551033575b0679840.

--- ARMeilleure/Instructions/InstEmitFlowHelper.cs
+++ ARMeilleure/Instructions/InstEmitFlowHelper.cs
@@ -246,7 +246,7 @@ namespace ARMeilleure.Instructions
             EmitNativeCall(context, fallbackAddr, isJump);
         }
 
-        private static void EmitDynamicTableCall(ArmEmitterContext context, Operand tableAddress, Operand address, bool isJump)
+        public static void EmitDynamicTableCall(ArmEmitterContext context, Operand tableAddress, Operand address, bool isJump)
         {
             // Loop over elements of the dynamic table. Unrolled loop.
 
